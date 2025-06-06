5b89de35cca128c8f24072d47837579a7b523be7
wfql1024 <wfql1024@gmail.com>
2025-06-06 21:31:34 +0800

    发布3.3.0.3718

--- Build4Win7.bat
+++ Build4Win7.bat
@@ -25,7 +25,7 @@ move /Y "dist\Updater.exe" "external_res\"
 :: 请先修改使用的虚拟环境，使用 PyInstaller 创建正常版本（无窗口）
 .venv37\Scripts\pyinstaller --name="极峰多聊" --windowed --icon=external_res\JFMC.ico ^
 --add-data="external_res;external_res" --distpath="dist\极峰多聊By37" ^
---version-file=.meta\version.txt --noconfirm Main.py
+--version-file=.meta\version.txt --noconfirm --hidden-import=comtypes.stream Main.py
 
 :: 检查打包是否成功
 if exist "dist\极峰多聊By37\极峰多聊\极峰多聊.exe" (
